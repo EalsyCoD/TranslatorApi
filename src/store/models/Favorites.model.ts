dee383727493f1ce7c91f8a1330bb59b65720c63
Константин <kanstantsin.rahalski@gmail.com>
2023-07-25 02:40:14 +0300

    removed models transition to inferValueTypes

--- src/store/models/Favorites.model.ts
+++ src/store/models/Favorites.model.ts
@@ -1,18 +1,5 @@
-import { FavoritesInitialState } from '../types';
-
 export enum EFavoritesActionType {
   SET_FAVORITES_SUCCESS = 'SET-FAVORITES-SUCCESS',
 
   GET_FAVORITES = 'GET-FAVORITES',
-}
-
-export interface IFavoritesSuccess {
-  type: EFavoritesActionType.SET_FAVORITES_SUCCESS;
-  payload: FavoritesInitialState['favorites'];
-}
-export interface IFavoritesGet {
-  type: EFavoritesActionType.GET_FAVORITES;
-  payload: FavoritesInitialState['favorites'];
-}
-
-export type TFavoritesType = IFavoritesSuccess | IFavoritesGet;
+};
--- src/store/models/Language.model.ts
+++ src/store/models/Language.model.ts
@@ -1,5 +1,3 @@
-import { LanguagesInitialState } from '../types';
-
 export enum ELanguageActionType {
   ALL_LANGUAGES = 'ALL-LANGUAGES',
 
@@ -11,37 +9,3 @@ export enum ELanguageActionType {
 
   SWAP_LANGUAGE = 'SWAP-LANGUAGE',
 }
-
-export interface ILanguagesSuccess {
-  type: ELanguageActionType.ALL_LANGUAGES;
-  payload: LanguagesInitialState;
-}
-export interface ILanguagesFROM {
-  type: ELanguageActionType.SET_LANGUAGE_FILTER_FROM;
-  payload: LanguagesInitialState;
-}
-
-export interface ILanguagesTO {
-  type: ELanguageActionType.SET_LANGUAGE_FILTER_TO;
-  payload: LanguagesInitialState;
-}
-export interface ILanguageSwap {
-  type: ELanguageActionType.SWAP_LANGUAGE;
-  payload: LanguagesInitialState;
-}
-export interface ISetTextFrom {
-  type: ELanguageActionType.SET_TEXT_AREA_FROM;
-  payload: LanguagesInitialState;
-}
-export interface ISetTextTo {
-  type: ELanguageActionType.SET_TEXT_AREA_TO;
-  payload: LanguagesInitialState;
-}
-
-export type TLanguagesType =
-  | ILanguagesSuccess
-  | ILanguagesFROM
-  | ILanguagesTO
-  | ILanguageSwap
-  | ISetTextFrom
-  | ISetTextTo
